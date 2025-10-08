ba49b3c16194c1286d81ec8d3d4515d32cc3358a
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-10-08 15:07:35 -0300

    f
    f

--- static/js/main.js
+++ static/js/main.js
@@ -18,13 +18,34 @@ function sumarCarrito() {
     etiquetaCantidadCarrito.innerText = carritoCantidad;
 }
 
+// Función sin parámetros para abrir la página de Burger King en AppStore
+function appStore() {
+    window.open("https://apps.apple.com/cl/app/burger-king-chile/id839816585");
+}
+
+// Función sin parámetros para abrir la página de Burger King en la Play Store
+function playStore() {
+    window.open("https://play.google.com/store/apps/details?id=com.crazycake.burgerkingchile&hl=es_CL");
+}
+
 // Alertas
+// Función sin parámetros para envíar una alerta con los detalles de Mi BK
+function detalles() {
+    alert("Si haces pedidos desde la aplicación, ganas coronas que puedes canjear por otros productos")
+}
+
+
 // Función sin parámetros para envíar una alerta de que la geolocalización está activada
 function geolocalActivar() {
     alert("Se ha activado la geolocalización!")
 }
 
-//Función sin parámetros para envíar una alerta de que se está descargando la app
+// Función sin parámetros para envíar una alerta de que se está descargando la app
 function descApp() {
     alert("Se está descargando la aplicación...")
+}
+
+// Función sin parámetros para envíar una alerta de que se está procesando tu pedido
+function pedido() {
+    alert("Se está procesando tu pedido...")
 }
